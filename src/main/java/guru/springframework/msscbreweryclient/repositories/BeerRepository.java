19d7e671ee443a58b8d0480127775b4beb74ddc1
Valeri Totev <valeri.totev@blankfactor.com>
2023-07-10 14:57:10 +0300

    added bootstrap and postgres config

--- src/main/java/guru/springframework/msscbreweryclient/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbreweryclient/repositories/BeerRepository.java
@@ -1,7 +1,6 @@
 package guru.springframework.msscbreweryclient.repositories;
 
 import guru.springframework.msscbreweryclient.domain.Beer;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
