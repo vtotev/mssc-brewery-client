a8fb081d357439c76468f5f2388580e63b64dbd1
Valeri Totev <valeri.totev@blankfactor.com>
2023-07-10 13:23:18 +0300

    adding beer repository

---
+++ src/main/java/guru/springframework/msscbreweryclient/repositories/BeerRepository.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbreweryclient.repositories;
+
+import guru.springframework.msscbreweryclient.domain.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+
+
+}
